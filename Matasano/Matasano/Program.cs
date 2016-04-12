51357c0e9a5e86cb33876eb4ee6b692dc75e57e2
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-12 10:30:58 -0700

    Attempt to solve1
    Needs to be redone

---
+++ Matasano/Matasano/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+            string converted = Set1.Challenge1.b64Encode(str);
+            Console.WriteLine(converted);
+        }
+    }
+}
---
+++ Matasano/Matasano/Set1/Challenge1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano.Set1
+{
+    class Challenge1
+    {
+        public static string toHexString(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            byte[] bytes = Encoding.Default.GetBytes(input);
+            foreach(var byteValue in bytes)
+            {
+                sb.Append(byteValue.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string b64Encode(string input)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(input);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
