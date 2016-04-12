a0eb631d488448480fb75fcc9b6edb0242637320
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-12 10:32:23 -0700

    Update missed file

--- Matasano/Matasano/Properties/AssemblyInfo.cs
+++ Matasano/Matasano/Properties/AssemblyInfo.cs
@@ -20,6 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02829703-5a6f-49a4-b854-f67db910284c")]
 
 // Version information for an assembly consists of the following four values:
 //
