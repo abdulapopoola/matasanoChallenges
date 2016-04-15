1e98da7b391611bcc4d5b75b60394b28659ec42a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-15 09:29:32 -0700

    Finished set1 in JS

--- Matasano JS/Set 1/challenge.js
+++ Matasano JS/Set 1/challenge.js
@@ -10,8 +10,7 @@ function getBytesFromHexString(hex) {
     let bufferLen = strLen / HEX_CHAR_LEN;
     let buffer = new ArrayBuffer(bufferLen);
     let bufferView = new Uint8ClampedArray(buffer);
-    for (let i = 0, j = 0; i < strLen; i += 2, j++)
-    {
+    for (let i = 0, j = 0; i < strLen; i += 2, j++) {
         let val = parseInt(hex.substr(i, 2), 16);
         bufferView[j] = val;
     }
@@ -19,4 +18,14 @@ function getBytesFromHexString(hex) {
     return bufferView;
 }
 
-var hexBytes = getBytesFromHexString('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
+function bufferToBase64(buffer) {
+    let binaryStr = String.fromCharCode.apply(String, buffer);
+    return btoa(binaryStr);
+}
+
+let hexBytes = getBytesFromHexString('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
+let encoded = bufferToBase64(hexBytes);
+if(encoded !== 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'){
+    throw new Error('Failed to convert to expected output');
+}
+console.log('SUCCESS!!!');
