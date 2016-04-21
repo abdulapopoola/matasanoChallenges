61d46f227b20f384ce0773e98e7968b105091c5a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-21 11:28:15 -0700

    Got it working; need to find auto ranker and also reuse modules

--- Matasano JS/Set 1/challenge3.js
+++ Matasano JS/Set 1/challenge3.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const UNIGRAM_FREQUENCIES = {
+    a: 8.04,
+    b: 1.48,
+    c: 3.34,
+    d: 3.82,
+    e: 12.49,
+    f: 2.40,
+    g: 1.87,
+    h: 5.05,
+    i: 7.57,
+    j: 0.16,
+    k: 0.54,
+    l: 4.07,
+    m: 2.51,
+    n: 7.23,
+    o: 7.64,
+    p: 2.14,
+    q: 0.12,
+    r: 6.28,
+    s: 6.51,
+    t: 9.28,
+    u: 2.73,
+    v: 1.05,
+    w: 1.68,
+    x: 0.23,
+    y: 1.66,
+    z: 0.09
+};
+
 function getByteValuesFromHexString(hex) {
     //returns integer values for bytes e.g. FF would be converted to 255
     const HEX_CHAR_LEN = 2; //hex strings are 2 chars long each
@@ -22,7 +51,7 @@ function getByteValuesFromHexString(hex) {
 function getHexStringForByteValuesArray(byteValuesArray) {
     //returns hex string for bytes values e.g. [255, 255] would be converted into 'FFFF'
     let hexValues = byteValuesArray.map(byteVal => (byteVal).toString(16));
-    return hexValues.join('');    
+    return hexValues.join('');
 }
 
 function XOR(byteArray1, byteArray2) {
@@ -39,24 +68,78 @@ function XOR(byteArray1, byteArray2) {
     return xoredBytes;
 }
 
-function getASCIIStringFromHexValues(hexValues){
-    return hexValues.map(hexVal => String.fromCharCode(hexVal)).join('');
+function getASCIIStringFromHexValues(hexValues) {
+    return String.fromCharCode.apply(String, hexValues).join('');
+    //return hexValues.map(hexVal => String.fromCharCode(hexVal)).join('');
 }
 
-function checkSuccess(expected, actual){
-    if(expected !== actual){
+function checkSuccess(expected, actual) {
+    if (expected !== actual) {
         throw new Error('Failed to give expected output. Expected: ' + expected + ' Actual: ' + actual);
     }
     console.log('GREAT JOB!!!');
 }
 
-let byteArray1 = getByteValuesFromHexString('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736');
-let byteArrLen = byteArray1.length;
-let hexChars = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-for(let i = 0; i < 16; i++){
-    let xorByteArr = new Array(byteArrLen);
-    xorByteArr.fill(hexChars[i]);
-    let xoredValues = XOR(byteArray1, xorByteArr);
-    let string = getASCIIStringFromHexValues(xoredValues);
-    console.log(string); 
+function getAllPossible() {
+    let byteArray1 = getByteValuesFromHexString('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736');
+    let byteArrLen = byteArray1.length;
+
+    let possibilities = [];
+    for (let i = 0; i < 256; i++) {
+        let xorByteArr = new Array(byteArrLen);
+        xorByteArr.fill(i);
+        let xoredValues = XOR(byteArray1, xorByteArr);
+        let string = getASCIIStringFromHexValues(xoredValues);
+        console.log(string);
+        possibilities.push(string);
+    }
+    
+    return possibilities;
 }
+
+function crossEntropy(str, freqArr) {
+    str = str.toLowerCase();
+    let sum = 0;
+    let nonAlphabetical = 0;
+    let len = str.length;
+    for (let i = 0; i < len; i++) {
+        let charIndex = str.charCodeAt(i);
+        if (charIndex >= 97 && charIndex <= 122) {
+            var charFreq = freqArr[charIndex - 97];
+            sum += log2(charFreq);
+        } else {
+            nonAlphabetical++;
+        }
+    }
+    return -(sum / (len - nonAlphabetical));
+}
+
+function decrypt() {
+    let normalizedFreqs = values(UNIGRAM_FREQUENCIES).map(freq => freq / 100);
+
+    let possibilities = getAllPossible();
+    let entropies = [];
+    for (let i = 0, len = possibilities.length; i < len; i++) {
+        let entropy = crossEntropy(possibilities[i], normalizedFreqs);
+        entropies.push([i, entropy]);
+    }
+
+    entropies.sort(function (x, y) {
+        // Compare by lowest entropy, break ties by lowest shift
+        if (x[1] < y[1]) return -1;
+        else if (x[1] > y[1]) return 1;
+        else if (x[0] < y[0]) return -1;
+        else if (x[0] > y[0]) return 1;
+        else return 0;
+    });
+
+    let bestAnswerIndex = entropies[0][0];
+    return possibilities[bestAnswerIndex];
+    //shift is also bestAnswerIndex
+}
+
+function values(obj){
+    return Object.keys(obj).map(key => obj[key]);
+}
+
+decrypt();
