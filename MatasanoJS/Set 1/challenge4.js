4c2705da664bf1f089de3819fadac9240370644a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-26 10:14:30 -0700

    Started on challenge 4

---
+++ MatasanoJS/Set 1/challenge4.js
@@ -0,0 +1,7 @@
+'use strict';
+const entropy = require('../Utils/entropy.js');
+const helpers = require('../Utils/helpers.js');
+
+//Need to read file
+//Decrypt each; use a big entropy bag
+//Grab the best one
